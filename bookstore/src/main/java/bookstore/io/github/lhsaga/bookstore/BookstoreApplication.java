de6a430a4c3d3f9c0ec0321cc54d0938cc1c3d37
liang <liuliang.liu@gmail.com>
2022-09-09 19:56:07 +0300

    first

---
+++ bookstore/src/main/java/bookstore/io/github/lhsaga/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package bookstore.io.github.lhsaga.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
