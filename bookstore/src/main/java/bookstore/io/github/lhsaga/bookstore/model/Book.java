59df5a8705e917bfcb21b0e86478a32f79289ca6
liang <liuliang.liu@gmail.com>
2022-09-09 22:58:45 +0300

    ex2

--- bookstore/src/main/java/bookstore/io/github/lhsaga/bookstore/model/Book.java
+++ bookstore/src/main/java/bookstore/io/github/lhsaga/bookstore/model/Book.java
@@ -1,5 +1,9 @@
 package bookstore.io.github.lhsaga.bookstore.model;
 
 public class Book {
-
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private double price;
 }
--- bookstore/src/main/java/bookstore/io/github/lhsaga/bookstore/web/BookController.java
+++ bookstore/src/main/java/bookstore/io/github/lhsaga/bookstore/web/BookController.java
@@ -1,5 +1,16 @@
 package bookstore.io.github.lhsaga.bookstore.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
 public class BookController {
+@RequestMapping("/index")
+public String listbook() {
+	return "Tang 100";
+}
+
 
 }
